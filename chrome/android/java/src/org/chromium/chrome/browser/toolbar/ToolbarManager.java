dece52f660056564f1054361ae32d27aea516ca8
Albert Wang <amwang217@gmail.com>
2019-02-22 09:39:15 -0800

    Fixes for incognito

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -225,7 +225,7 @@ public class ToolbarManager
     private boolean mInitializedWithNative;
     private Runnable mOnInitializedRunnable;
 
-    private final boolean mShouldUpdateToolbarPrimaryColor = false;
+    private boolean mShouldUpdateToolbarPrimaryColor;
     private int mCurrentThemeColor;
 
     private OmniboxStartupMetrics mOmniboxStartupMetrics;
@@ -1473,8 +1473,7 @@ public class ToolbarManager
      *                     from the Tab.
      */
     public void setShouldUpdateToolbarPrimaryColor(boolean shouldUpdate) {
-        // (Albert Wang): Disabled since we have locked toolbar colors
-        // mShouldUpdateToolbarPrimaryColor = shouldUpdate;
+        mShouldUpdateToolbarPrimaryColor = shouldUpdate;
     }
 
     /**
