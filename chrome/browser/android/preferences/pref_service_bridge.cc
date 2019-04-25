remzert/BraveBrowser

Added HTTPSE support
Auditors: @diracdeltas

chrome/browser/android/preferences/pref_service_bridge.cc
@@ -351,6 +351,12 @@ static void SetSafeBrowsingEnabled(JNIEnv* env,
   GetPrefService()->SetBoolean(prefs::kSafeBrowsingEnabled, enabled);
 }
 
+static void SetHTTPSEEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kHTTPSEEnabled, enabled);
+}
+
 static void SetAdBlockEnabled(JNIEnv* env,
                                    const JavaParamRef<jobject>& obj,
                                    jboolean enabled) {

chrome/browser/profiles/profile_io_data.cc
@@ -476,6 +476,7 @@ void ProfileIOData::InitializeOnUIThread(Profile* profile) {
   ChromeNetworkDelegate::InitializePrefsOnUIThread(
       &enable_referrers_,
       &enable_do_not_track_,
+      &enable_httpse_,
       &enable_tracking_protection_,
       &enable_ad_block_,
       &force_google_safesearch_,
@@ -1068,6 +1069,7 @@ void ProfileIOData::Init(
   network_delegate->set_profile_path(profile_params_->path);
   network_delegate->set_cookie_settings(profile_params_->cookie_settings.get());
   network_delegate->set_enable_do_not_track(&enable_do_not_track_);
+  network_delegate->set_enable_httpse(&enable_httpse_);
   network_delegate->set_enable_tracking_protection(&enable_tracking_protection_);
   network_delegate->set_enable_ad_block(&enable_ad_block_);
   network_delegate->set_force_google_safe_search(&force_google_safesearch_);
@@ -1288,6 +1290,7 @@ void ProfileIOData::ShutdownOnUIThread(
   google_services_user_account_id_.Destroy();
   enable_referrers_.Destroy();
   enable_do_not_track_.Destroy();
+  enable_httpse_.Destroy();
   enable_tracking_protection_.Destroy();
   enable_ad_block_.Destroy();
   force_google_safesearch_.Destroy();

redy88/browser-android-tabs

Conflicts:
	chrome/browser/net/chrome_network_delegate.cc
	chrome/android/java/strings/android_chrome_strings.grd

chrome/browser/android/preferences/pref_service_bridge.cc
@@ -443,6 +443,12 @@ static void JNI_PrefServiceBridge_SetSafeBrowsingEnabled(
   GetPrefService()->SetBoolean(prefs::kSafeBrowsingEnabled, enabled);
 }
 
+static void JNI_PrefServiceBridge_SetHTTPSEEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kHTTPSEEnabled, enabled);
+}
+
 static void JNI_PrefServiceBridge_SetAdBlockEnabled(JNIEnv* env,
                                    const JavaParamRef<jobject>& obj,
                                    jboolean enabled) {

chrome/browser/profiles/profile_io_data.cc
@@ -438,6 +438,7 @@ void ProfileIOData::InitializeOnUIThread(Profile* profile) {
       base::CreateSingleThreadTaskRunnerWithTraits({BrowserThread::IO}));
 
   ChromeNetworkDelegate::InitializePrefsOnUIThread(
+      &enable_httpse_,
       &enable_tracking_protection_,
       &enable_ad_block_
       pref_service);
@@ -949,6 +950,7 @@ void ProfileIOData::Init(
         profile_params_->cookie_settings.get());
     chrome_network_delegate->set_force_google_safe_search(
         &force_google_safesearch_);
+    chrome_network_delegate->set_enable_httpse(&enable_httpse_);
     chrome_network_delegate->set_enable_tracking_protection(&enable_tracking_protection_);
     chrome_network_delegate->set_enable_ad_block(&enable_ad_block_);
 
@@ -1166,6 +1168,7 @@ void ProfileIOData::ShutdownOnUIThread(
 #if !defined(OS_CHROMEOS)
   signin_scoped_device_id_.Destroy();
 #endif
+  enable_httpse_.Destroy();
   enable_tracking_protection_.Destroy();
   enable_ad_block_.Destroy();
   force_google_safesearch_.Destroy();