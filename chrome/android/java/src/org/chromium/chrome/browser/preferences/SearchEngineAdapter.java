remzert/BraveBrowser

Sort search engines alphabetically

chrome/android/java/src/org/chromium/chrome/browser/preferences/SearchEngineAdapter.java
@@ -25,6 +25,7 @@ import android.widget.TextView;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ContextUtils;
+import org.chromium.base.Log;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.omnibox.geo.GeolocationHeader;
 import org.chromium.chrome.browser.preferences.website.ContentSetting;
@@ -38,6 +39,7 @@ import org.chromium.components.location.LocationUtils;
 import org.chromium.ui.text.SpanApplier;
 import org.chromium.ui.text.SpanApplier.SpanInfo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -117,7 +119,23 @@ public class SearchEngineAdapter extends BaseAdapter implements LoadListener, On
         }
 
         // Fetch all the search engine info and the currently active one.
-        mSearchEngines = templateUrlService.getLocalizedSearchEngines();
+        List<TemplateUrl> searchEnginesUnsorted = templateUrlService.getLocalizedSearchEngines();
+        mSearchEngines = new ArrayList<TemplateUrl>();
+        for (int i = 0; i < searchEnginesUnsorted.size(); ++i) {
+            boolean added = false;
+            String currentShortName = searchEnginesUnsorted.get(i).getShortName();
+            for (int j = 0; j < mSearchEngines.size(); ++j) {
+                String shortName = mSearchEngines.get(j).getShortName();
+                if (shortName.compareTo(currentShortName) > 0) {
+                    mSearchEngines.add(j, searchEnginesUnsorted.get(i));
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                mSearchEngines.add(searchEnginesUnsorted.get(i));
+            }
+        }
         int searchEngineIndex = templateUrlService.getDefaultSearchEngineIndex();
         // Convert the TemplateUrl index into an index into mSearchEngines.
         mSelectedSearchEnginePosition = -1;

redy88/browser-android-tabs

Conflicts:
	chrome/android/java/src/org/chromium/chrome/browser/preferences/SearchEngineAdapter.java

chrome/android/java/src/org/chromium/chrome/browser/preferences/SearchEngineAdapter.java
@@ -160,15 +160,31 @@ public class SearchEngineAdapter extends BaseAdapter
             return;  // Flow continues in onTemplateUrlServiceLoaded below.
         }
 
-        List<TemplateUrl> templateUrls = templateUrlService.getTemplateUrls();
+        List<TemplateUrl> templateUrls = new ArrayList<TemplateUrl>();
+        List<TemplateUrl> templateUrlsUnsorted = templateUrlService.getSearchEngines();
         TemplateUrl defaultSearchEngineTemplateUrl =
                 templateUrlService.getDefaultSearchEngineTemplateUrl();
         sortAndFilterUnnecessaryTemplateUrl(templateUrls, defaultSearchEngineTemplateUrl);
         boolean forceRefresh = mIsLocationPermissionChanged;
         mIsLocationPermissionChanged = false;
-        if (!didSearchEnginesChange(templateUrls)) {
+        if (!didSearchEnginesChange(templateUrlsUnsorted)) {
             if (forceRefresh) notifyDataSetChanged();
             return;
+        }
+        for (int i = 0; i < templateUrlsUnsorted.size(); ++i) {
+            boolean added = false;
+            String currentShortName = templateUrlsUnsorted.get(i).getShortName();
+            for (int j = 0; j < templateUrls.size(); ++j) {
+                String shortName = templateUrls.get(j).getShortName();
+                if (shortName.compareTo(currentShortName) > 0) {
+                    templateUrls.add(j, templateUrlsUnsorted.get(i));
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                templateUrls.add(templateUrlsUnsorted.get(i));
+            }
         }
 
         mPrepopulatedSearchEngines = new ArrayList<>();